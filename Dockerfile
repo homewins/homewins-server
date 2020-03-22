8aec9a1486e3ec359468158bf7a93c4374f1708d
Vinh Ngu <vinh-ngu@hotmail.com>
2020-03-22 13:43:07 +0100

    Add production files.

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,20 @@ FROM python:3.6
 
 LABEL maintainer="vinh-ngu@hotmail.com"
 
+# Install docker
+RUN apt update && \
+	apt install -y  \
+        curl \
+        nginx \
+        gettext
+
 WORKDIR /app
 ADD . .
-RUN chmod +x entrypoint.sh
 RUN pip install -r requirements.txt
+RUN chmod +x entrypoint.sh
 
-EXPOSE 8000
+# Forward request and error logs to docker log collector
+RUN ln -sf /dev/stdout /var/log/nginx/access.log \
+    && ln -sf /dev/stderr /var/log/nginx/error.log
+
 CMD ["./entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
+set -e
+
+echo "Migrate files..."
 python manage.py migrate
-python manage.py runserver
+
+# Create static files.
+python manage.py collectstatic --clear --noinput
+
+if [ "$TYPE" = 'worker' ]; then
+
+    echo "Run gunicorn..."
+    gunicorn --bind 0.0.0.0:8800 newsbox.wsgi:application --access-logfile -
+elif [ "$TYPE" = 'master' ]; then
+    env | sed "s/\([^=]*\)=\(.*\)/export\ \1='\2'/" > envs.sh
+
+    echo "Render nginx.conf..."
+    envsubst '\$HOMEWINS_API_WORKER_HOST' < nginx.conf.template > nginx.conf
+
+    echo "Run nginx..."
+    cp -r static/ /usr/share/nginx/html
+    cp -r nginx.conf /etc/nginx/conf.d/default.conf
+    nginx -g 'daemon off;'
+else
+    echo "Set env TYPE to master/worker."
+    exit 1
+fi
